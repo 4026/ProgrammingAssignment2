f4e756e92777fbf6790b83a067946a3f7873c92c
Ben Hallett <name4026@gmail.com>
2017-04-29 16:41:05 +0100

    Submitting solution for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to create a special matrix class that caches the value of its 
+## inverse, and a function that calculates the inverse of such a matrix or 
+## returns a cached value if it was already calculated.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Create a special type of matrix that is able to cache the value of its 
+## inverse.
+makeCacheMatrix <- function(m = matrix()) {
+    inverse <- NULL
+    set <- function(new_m) {
+        m <<- new_m
+        inverse <<- NULL
+    }
+    get <- function() m
+    setinverse <- function(new_inverse) inverse <<- new_inverse
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Returns a matrix that is the inverse of 'm', which is an instance of the 
+## special matrix class created by the makeCacheMatrix function. If the provided
+## matrix has had its inverse claculated by this function before, a cached 
+## result will be returned instead of recalulating the inverse.
+cacheSolve <- function(m, ...) {
+    cached_value <- m$getinverse()
+    if(!is.null(cached_value)) {
+        message("getting cached data")
+        return(cached_value)
+    }
+    data <- m$get()
+    
+    ## We disregard any additional arguments passed to cacheSolve, rather than 
+    ## passing them through to solve, because passing through additional 
+    ## arguments might cause us to get an output from solve that is not the 
+    ## inverse of the provided matrix, which would in turn lead us to cache that
+    ## value and return it for later calls to cacheSolve, which would then also 
+    ## be rendered incorrect.
+    inverse <- solve(data) 
+    m$setinverse(inverse)
+    inverse
 }
